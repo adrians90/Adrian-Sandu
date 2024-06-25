70fc020b36a45a4e694bd612bfbb48231246070b
adrians90 <adriansandu9090@gmail.com>
2024-06-25 14:47:15 +0100

    Style the contact form button

--- components/contact.tsx
+++ components/contact.tsx
@@ -19,9 +19,10 @@ export default function Contact() {
         <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
         <button
           type="submit"
-          className="flex items-center justify-center gap-2 h-[3rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110  active:scale-105 hover:bg-gray-950"
         >
-          Submit <FaPaperPlane className="text-xs opacity-70 transition-all" />
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </button>
       </form>
     </section>
