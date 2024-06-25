841f02f8d3fc21f72bacda3dae887168dddeae05
adrians90 <adriansandu9090@gmail.com>
2024-06-25 13:55:00 +0100

    Cleared the components and used custom hook useSectionInView

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,18 +3,18 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import type { SectionName } from "@/lib/types";
 
-export function useSectionInView(sectionName: SectionName) {
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { ref, inView } = useInView({
-    threshold: 0.5,
+    threshold,
   });
 
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
     if (inView && Date.now() - timeOfLastClick > 1000) {
-      setActiveSection("Home");
+      setActiveSection(sectionName);
     }
-  }, [inView, setActiveSection, timeOfLastClick]);
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
 
   return {
     ref,
