6cbc758e1340af59037b260500670dc32be77f99
adrians90 <adriansandu9090@gmail.com>
2024-06-25 15:17:37 +0100

    Extract sendEmail logic into own server action component

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,9 +5,11 @@ import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+
   return (
     <motion.section
       ref={ref}
@@ -33,15 +35,21 @@ export default function Contact() {
         or through this form.{" "}
       </p>
 
-      <form className="mt-10 flex flex-col">
+      <form className="mt-10 flex flex-col" action={sendEmail}>
         <input
           className="h-14 px-4 rounded-lg borderBlack"
+          name="senderEmail"
           type="email"
+          required
+          maxLength={500}
           placeholder="Your email"
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4"
+          name="message"
           placeholder="Your message"
+          required
+          maxLength={500}
         />
         <button
           type="submit"
