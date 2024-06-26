1a2d48f777cda3a405aac87fa7b749b77a9e27a7
adrians90 <adriansandu9090@gmail.com>
2024-06-26 11:57:26 +0100

    Save theme preference in local  storage

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { BsSun } from "react-icons/bs";
+"use client";
+
+import React, { useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
+
+type Theme = "light" | "dark";
 
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  };
   return (
     <button
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacirt40
      shadow-xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
     >
-      <BsSun />
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
